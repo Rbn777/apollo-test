383adfb2d329477f380d318170a19937c24b3572
Robin <robin.dorion@gmail.com>
2020-11-12 11:28:03 +0100

    adding apollo and graphql depedencies, and displaying 5 launches

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import { ApolloClient, ApolloProvider, InMemoryCache, useQuery, gql } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'https://api.spacex.land/graphql/',
+  cache: new InMemoryCache()
+});
+
+const SPACEX_LAUNCHES = gql`
+  query GetLaunches {
+    launches(limit: 5) {
+      launch_date_utc
+      launch_success
+      rocket {
+        rocket_name
+      }
+      links {
+        video_link
+      }
+      details
+      id
+    }
+  }
+`;
+
+function SpaceXLaunches() {
+  const { loading, error, data } = useQuery(SPACEX_LAUNCHES);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  return data.launches.map(({ rocket, details, launch_date_utc, launch_success, id }) => (
+    <div key={id} className='LaunchCard'>
+      <h3>
+        date: {launch_date_utc}, success: {launch_success? 'Yes' : 'No'}
+      </h3>
+      <h5>Rocket: {rocket.rocket_name}</h5>
+      <p>{details}</p>
+    </div>
+  ));
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <div className='Container'>
+        <SpaceXLaunches />
+      </div>
+    </ApolloProvider>
   );
 }
 
